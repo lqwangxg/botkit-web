5e176bf17270cbcb4ea0cd39b07f74a8d3cab409
lqwangxg@gmail.com <U0Bd9Oa4>
2020-06-03 18:32:15 +0900

    movie details

--- skills/unhandled_messages.js
+++ skills/unhandled_messages.js
@@ -2,7 +2,32 @@ module.exports = function(controller) {
 
   controller.on('message_received', function(bot, message) {
     console.debug(message);
-    
+    if(message.action === "get-movie-info" ){
+      controller.trigger();
+      const movie = movie_service.getDetail(context.confirmed.movie.toString());
+      const text =`${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+      
+      const jsonBody = { 
+        "fulfillmentText": text,
+        "payload": {
+          "google": {
+            "expectUserResponse": true,
+            "richResponse": {
+              "items": [
+                {
+                  "simpleResponse": {
+                    "textToSpeech": text
+                  }
+                }
+              ]
+            }
+          }
+        }
+      };
+      bot.reply(message, jsonBody);
+      return; 
+    }
+
     bot.reply(message, {
         text: 'I do not know how to respond to that message yet.  Define new features by adding skills in my `skills/` folder.  [Read more about building skills](https://github.com/howdyai/botkit-starter-web/blob/master/docs/how_to_build_skills.md).\n\n(This message is from the unhandled_messages skill.)',
         quick_replies: [
