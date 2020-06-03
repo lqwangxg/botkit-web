8a5c901ac70b3e480a0b6885457868a41f1bc7f1
lqwangxg@gmail.com <U0Bd9Oa4>
2020-06-03 19:39:04 +0900

    bulebird

--- service/movie-service.js
+++ service/movie-service.js
@@ -6,11 +6,11 @@ const request = require('request');
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 const URL_BASE = `http://www.omdbapi.com/?apikey=${MOVIE_API_KEY}`;
 
-//Promise = require('bluebird');
-//Promise.promisifyAll(request);
+Promise = require('bluebird');
+Promise.promisifyAll(request);
 
 module.exports = class MovieService {
-
+  
     static getDetail(movieTitle){
         let url = encodeURI(URL_BASE + "&t="+ movieTitle);
         let headers = {
