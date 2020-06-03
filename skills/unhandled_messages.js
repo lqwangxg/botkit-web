41d9823bd6ce359659df6385553e46b6562eee73
lqwangxg@gmail.com <U0Bd9Oa4>
2020-06-03 17:46:40 +0900

    Revert "Revert "Revert "commit"""
    This reverts commit 69635caabc6bb11464c93aaa2352f8aa22bb29f9.

--- skills/unhandled_messages.js
+++ skills/unhandled_messages.js
@@ -3,12 +3,14 @@ module.exports = function(controller) {
   controller.hears(['Default Welcome Intent'], 'direct_message', function(bot, message) {
     replyText = message.fulfillment.text;  // message object has new fields added by Dialogflow
     bot.reply(message, replyText);
-    console.log(`controller.hears message:${message}`);
+    console.debug(`controller.hears message:${message}`);
   });
   
   controller.on('message_received', function(bot, message) {
-    console.log(message);
-    console.log(`intent:${message.intent}`);
+    console.debug(message);
+    console.debug(`intent:${message.intent}`);
+    console.debug(`parameters:${message.parameters}`);
+    console.debug(`diagnosticInfo:${message.diagnosticInfo}`);
     
     bot.reply(message, {
       text: 'I do not know how to respond to that message yet.  Define new features by adding skills in my `skills/` folder.  [Read more about building skills](https://github.com/howdyai/botkit-starter-web/blob/master/docs/how_to_build_skills.md).\n\n(This message is from the unhandled_messages skill.)',
